9115244264c018e462d5c1b4dcc1f371ff990484
Dhruv Singh <dhruvsingh7077@gmail.com>
2025-11-10 18:56:21 +0530

    added the logos and sign up

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import Image from 'next/image';
+const Layout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div className="flex min-h-screen">
+      <section className="bg-brand p-10">
+        <div>
+          <Image
+            src="/assets/icons/logo-full.svg"
+            alt="logo"
+            width={224}
+            height={82}
+            className="h-auto"
+          />
+
+          <div className="space-y-5 text-white">
+            <h1 className="h1">Manage your files the best way</h1>
+            <p className="body-1">This is a place to tore All your files</p>
+          </div>
+        </div>
+      </section>
+      <section className="flex-1 p-10">{children}</section>
+    </div>
+  );
+};
+
+export default Layout;

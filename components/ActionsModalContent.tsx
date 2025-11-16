10e279bc0053a73ec3c8a66e61005a32079d54a1
Dhruv Singh <dhruvsingh7077@gmail.com>
2025-11-16 23:11:02 +0530

    written file actions

--- components/ActionsModalContent.tsx
+++ components/ActionsModalContent.tsx
@@ -3,6 +3,10 @@ import { Models } from 'node-appwrite';
 import Thumbnail from '@/components/Thumbnail';
 import FormattedDateTime from '@/components/FormattedDateTime';
 import { convertFileSize, formatDateTime } from '@/lib/utils';
+import { Input } from 'postcss';
+import { separator } from 'path/separator';
+import { Button } from '@/components/ui/button';
+import Image from 'next/image';
 
 const ImageThumbnail = ({ file }: { file: Models.Document }) => (
   <div className="file-details-thumbnail">
@@ -40,8 +44,48 @@ interface Props {
   onInputChange: React.Dispatch<React.SetStateAction<string[]>>;
   onRemove: (email: string) => void;
 }
-const ShareInput = ({ file, onInputChance, onRemove }: Props) => {
-  return <div>ShareInput</div>;
-};
+ export const ShareInput = ({ file, onInputChance, onRemove }: Props) => {
+  return (
+    <>
+    <ImageThumbnail file={file} />
+    <div className='share-wrapper'>
+      <p className='subtitle-2 pl-1 text-light-100'>
+        Share file with other users
+      </p>
+      <Input
+      type="email"
+      placeholder="Enter email address"
+      onChange={(e) => onInputChange(e.target.value.trim().split(separator:""))}
+      className="share-input-field"
+      />
+      <div className='pt-4'>
+        <div className='flex justify-between'>
+          <p className='caption text-light-200'>Shared with</p>
+          <p className='caption text-light-200'>{file.users.length} users</p>
+        </div>
 
+        <ul className='pt-2'>
+          {file.users.map((email:string)) => (
+             <li
+             key={email}
+             className='flex items-center jusify-between gap-2'>
+              <p className='subtitle-2'>{email}</p>
+              <Button onClick={() => onRemove(email)}
+                className='share-remove-user'>
+                <Image
+                  src="/assets/icons/remove.svg"
+                  alt="Remove "
+                  width={24}
+                  height={24}
+                  className="remove-icon"
+                />
+              </Button>
+             </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+    </>
+  )
+};
 export default ShareInput;
